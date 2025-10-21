a187a4839e901b9d0428ce169018f9e7fa0999d1
Yash Verma <yashverma056@proton.me>
2025-10-21 10:21:08 -0400

    Add Class Capsule

---
+++ src/modules/class_capsule.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+from ..utils.squash import squash
+
+
+class ClassCapsule(nn.Module):
+    """The class capsules layer which performs routing-by-agreement."""
+    def __init__(
+            self,
+            num_capsules: int,
+            num_routes: int,
+            in_dim: int,
+            out_dim: int,
+            num_routing: int = 3,
+    ) -> None:
+        super().__init__()
+        self.num_routing: int = num_routing
+        self.vote: torch.Tensor = nn.Parameter(
+            torch.randn(
+                1,
+                num_routes,
+                num_capsules,
+                out_dim,
+                in_dim
+            )
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.unsqueeze(2).unsqueeze(3)
+        votes: torch.Tensor = torch.matmul(self.vote, x)
+        agreement: torch.Tensor = torch.zeros_like(votes)
+        verdict: torch.Tensor = torch.Tensor()
+
+        for _ in range(self.num_routing):
+            chairman: torch.Tensor = torch.softmax(agreement, dim=1)
+            summary: torch.Tensor = (chairman * votes).sum(dim=1, keepdim=True)
+            verdict = squash(summary)
+            agreement += (votes * verdict).sum(dim=3, keepdim=True)
+
+        return verdict.squeeze(1).squeeze(-1)
